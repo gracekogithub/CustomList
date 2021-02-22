3380a179f94a68bc0e10e09f98249a6b91d80621
gracekogithub <graceko02011972@gmail.com>
2021-02-22 09:45:43 -0800

    finished first add method in AddMethodUnitTest1 class that count increases by one

---
+++ CustomListUnitTesting/AddMethodUnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListNew;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class AddMethodUnitTest1
+    {
+
+        [TestMethod]
+        public void Add_CountIncreases_ByOne()
+        {
+            //Arange
+            CustomList<int> listOfNumbers = new CustomList<int>();
+            int number = 9;
+            int expectedResult = 1;
+            int actualResult;
+
+            //Act
+            listOfNumbers.Add(number);
+            actualResult = listOfNumbers.Count;
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
---
+++ CustomListUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListUnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6053aa52-d095-4df7-a0a6-91ed6cafb6d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
